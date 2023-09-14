a7dde394b94183b3cb4ab316d36c7ba6c4ca1798
fabianluethi <fabian.luethi@gmail.com>
2023-09-14 11:34:51 +0200

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -7,6 +7,9 @@ rm -f /run/rsyslogd.pid
 # start service
 service rsyslog start
 
+# Reconfigure for right driver
+"${WEEWX_HOME}"/bin/wee_config --reconfigure --driver=user.gw1000
+
 # start weewx
 echo 'Starting weewx'
 "${WEEWX_HOME}"/bin/weewxd /home/weewx/weewx.conf
